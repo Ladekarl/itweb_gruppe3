2a7562241ab7ac39724287cef1bc379fdb3e0e12
Mathis Møller <thisen-dk@hotmail.com>
2016-11-10 10:37:53 +0100

    Created GET trainingPrograms and fixed routes

--- Exercise2/app.js
+++ Exercise2/app.js
@@ -9,6 +9,7 @@ require('./app_api/models/db');
 var index = require('./app_server/routes/index');
 var exercises = require('./app_server/routes/exercises');
 var programs = require('./app_server/routes/programs');
+var api = require('./app_api/routes/index');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/exercises', exercises);
 app.use('/trainingProgram/new', programs);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- Exercise2/app_server/routes/index.js
+++ Exercise2/app_server/routes/index.js
@@ -1,12 +1,26 @@
 var express = require('express');
-var db = require("../../app_api/models/db.js");
+var api = require("../../app_api/models/db.js");
 var router = express.Router();
+var request = require('request');
+
+var requestOptions = {
+  url : "http://localhost:3000/api/trainingPrograms",
+  method : "GET",
+  json : {}
+};
+
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  db.program.find({}, function (err, programs) {
-    res.render('index', {trainingPrograms: programs});
-  });
+  request(requestOptions, function(err, response, body) {
+    if (err) {
+      console.log(err);
+    } else if (response.statusCode === 200) {
+      res.render('index', {trainingPrograms: body});
+    } else {
+      console.log(response.statusCode);
+    }
+  })
 });
 
 module.exports = router;
