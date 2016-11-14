140ba094ca511bd322a2fa4ea58c5f537ff1c363
Mathis Møller <thisen-dk@hotmail.com>
2016-11-14 15:12:38 +0100

    Added PUT and DELETE to training programs

--- Exercise2/app_api/routes/index.js
+++ Exercise2/app_api/routes/index.js
@@ -4,6 +4,8 @@ var ctrlTrainingPrograms = require('../controllers/trainingPrograms');
 
 router.get('/trainingPrograms', ctrlTrainingPrograms.trainingProgramsByUser);
 router.post('/trainingPrograms/:name', ctrlTrainingPrograms.postTrainingProgramsByUser);
+router.put('/trainingPrograms/:id', ctrlTrainingPrograms.updateTrainingProgramsByUser);
+router.delete('/trainingPrograms/:id', ctrlTrainingPrograms.deleteTrainingProgramsByUser);
 router.patch('/trainingPrograms/:id', ctrlTrainingPrograms.patchTrainingProgramsByUser);
 
 module.exports = router;
