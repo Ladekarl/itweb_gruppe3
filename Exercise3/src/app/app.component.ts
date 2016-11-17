21767a976f615b6bdc84226d88ae270d367c6348
Niels Ladekarl <nielsladekarl@gmail.com>
2016-11-17 10:49:15 +0100

    proxy to local

--- Exercise3/src/app/app.component.ts
+++ Exercise3/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Http} from "@angular/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
+
+  constructor(private http: Http) {
+    this.http.get('/api/')
+      .map(res => res.text())
+      .subscribe(
+        data => this.title = data,
+        err => this.title = err,
+        () => console.log('Random Quote Complete')
+      );
+  }
+
   title = 'app works!';
 }
