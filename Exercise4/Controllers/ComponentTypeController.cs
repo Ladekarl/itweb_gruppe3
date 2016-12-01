f83540357adba805cb6283ae574ce5505fe17b44
Thisen <thisen-dk@hotmail.com>
2016-12-01 15:41:14 +0100

    Added filtering of component types by category

--- Exercise4/Controllers/ComponentTypeController.cs
+++ Exercise4/Controllers/ComponentTypeController.cs
@@ -3,21 +3,47 @@ using Exercise4.Services;
 using Exercise4.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Exercise4.Controllers
 {
     public class ComponentTypeController : Controller
     {
-        private IComponentTypeData _componentDataType;
-        public ComponentTypeController(IComponentTypeData componentDataType)
+        private readonly IComponentTypeData _componentDataType;
+        private readonly ICategoryData _categoryData;
+
+        public ComponentTypeController(IComponentTypeData componentDataType, ICategoryData categoryData)
         {
             _componentDataType = componentDataType;
+            _categoryData = categoryData;
         }
 
         public IActionResult Index()
         {
-            var model = new ComponentTypeViewModel();
-            model.Types = _componentDataType.GetAll();
+            var categories = _categoryData.GetAll().Select(x => new SelectListItem
+            {
+                Value = x.CategoryId.ToString(),
+                Text = x.Name
+            });
+
+            var selectListItems = categories as IList<SelectListItem> ?? categories.ToList();
+
+            selectListItems.Add(new SelectListItem
+            {
+                Text = "All",
+                Value = 0.ToString(),
+                Selected = true
+            });
+
+            var model = new ComponentTypeViewModel
+            {
+                Types = _componentDataType.GetAll(),
+                Categories = selectListItems
+            };
+
             return View(model);
         }
 
@@ -45,6 +71,44 @@ namespace Exercise4.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View();
+        }
+
+        public IActionResult Filter([FromQuery] int categoryId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            if (categoryId == 0)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            var componentTypes = _componentDataType.GetAll().Where(
+                c => c.Categories.FirstOrDefault(cat => cat.CategoryId == categoryId) != null);
+
+            var categories = _categoryData.GetAll().Select(x => new SelectListItem
+            {
+                Value = x.CategoryId.ToString(),
+                Text = x.Name,
+                Selected = x.CategoryId == categoryId
+            });
+
+            var selectListItems = categories as IList<SelectListItem> ?? categories.ToList();
+
+            selectListItems.Add(new SelectListItem
+            {
+                Text = "All",
+                Value = 0.ToString()
+            });
+
+            var ctvm = new ComponentTypeViewModel
+            {
+                Types = componentTypes.ToList(),
+                Categories = selectListItems
+            };
+            return View(nameof(Index), ctvm);
         }
     }
 }
--- Exercise4/ViewModels/ComponentTypeViewModel.cs
+++ Exercise4/ViewModels/ComponentTypeViewModel.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
 using Exercise4.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Exercise4.ViewModels
 {
     public class ComponentTypeViewModel
     {
         public IEnumerable<ComponentType> Types{ get; set; }
+
+        public IEnumerable<SelectListItem> Categories { get; set; }
     }
 }
